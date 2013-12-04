c3c3364a6413d7ea912a3a5e74e00865f5803ae9
Joseph Perla <noootreeeeall@**jperla.com**>
2013-12-04 12:05:55 -0800

    added sgd classifier

--- reproduce_roc_2008.py
+++ reproduce_roc_2008.py
@@ -9,9 +9,10 @@ import numpy as np
 
 import logistic
 
-#C_VALUES = [0.00001, 0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0, 100000.0]
+#C_VALUES = [0.01, 0.1]
 #C_VALUES = [1e-13, 1e-10, 1e-7, 1e-4, 1e-1, 1e2, 1e5, 1e8, 1e11, 1e14]
-C_VALUES = [0.01, 0.1]
+#C_VALUES = [1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3]
+C_VALUES = [2**-8, 2**-7, 2**-6, 2**-5, 2**-4, 2**-3,]
 
 def read_swissprot_data():
     """Reads in swissprot dataset from 3 files in proteindata folder.
@@ -38,41 +39,31 @@ def calculate_roc(true_labels, estimated_labels):
     roc_auc = sklearn.metrics.auc(fpr, tpr)
     return fpr, tpr, roc_auc
 
-def validate_svm(train_set, train_labels, validation_set, validation_labels, C=1000.0, CP=1000.0, sample_weight=None):
-    positive_weight = CP / C
-    classifier = sklearn.svm.SVC(kernel='linear',
-                                 C=C,
-                                 class_weight={0: 1.0, 1: positive_weight})
-    classifier.fit(train_set, train_labels, sample_weight=sample_weight)
-    classifier.probability = True
-    svm_labels = classifier.predict(validation_set)
-    accuracy = float(sum(validation_labels == svm_labels)) / len(svm_labels)
-    return classifier, accuracy
-
 def svm_label_data(train_set, train_labels, test_set, C=C_VALUES, CP=None, sample_weight=None):
     """Tries out several values for C by training on 75% of the training data, and validating on the rest.
         Picks the best values of C then uses it on the test set.
     """
-    kfolds = list(sklearn.cross_validation.KFold(train_set.shape[0], k=3, shuffle=True, random_state=0))
-
     svms = []
     for c in C:
         cps = [c] if CP is None else CP
         for cp in cps:
             svm, accuracies = None, []
 
-            svm = sklearn.svm.SVC(kernel='linear', C=c, class_weight={0: 1.0, 1: cp / c})
+            svm = sklearn.svm.LinearSVC(C=c, class_weight={0: 1.0, 1: cp / c})
             svm.probability = True
-            scores = sklearn.cross_validation.cross_val_score(svm, train_set, train_labels, cv=3, n_jobs=-1)
-
+            scores = sklearn.cross_validation.cross_val_score(svm, train_set, train_labels, 
+                                                              cv=3, n_jobs=6,
+                                                              #scoring='roc_auc',
+                                       )#                       fit_params={'sample_weight': sample_weight})
             accuracy = scores.mean()
+
             svms.append((accuracy, 1.0 / c, c, cp, svm))
-            logging.debug('accuracies = %s' % str(scores))
             logging.debug('svm C=%s, CP=%s: %.2f%%' % (c, cp, 100 * accuracy))
 
     # get the top SVM
     best_svm = list(reversed(sorted(svms)))[0]
     logging.info('Best SVM: C=%s, CP=%s, accuracy=%.2f' % (best_svm[2], best_svm[3], best_svm[0]))
+
     svm = sklearn.svm.SVC(kernel='linear', C=best_svm[2], class_weight={0: 1.0, 1: best_svm[3] / best_svm[2]})
     svm.probability = True
 
@@ -89,16 +80,18 @@ if __name__=='__main__':
 
     pos, neg, unlabeled_pos = read_swissprot_data()
     # switch cases to use a smaller labeled dataset
-    unlabeled_pos, pos = pos, unlabeled_pos 
+    if 'switch_cases' in locals() and switch_cases:
+        logging.warning('Switching positive and negative datasets!')
+        unlabeled_pos, pos = pos, unlabeled_pos 
 
     # Use less data so that we can move faster, comment this out to use full dataset
     #truncate = lambda m: m[:int(m.shape[0] / 30),:]
     #pos, neg, unlabeled_pos = truncate(pos), truncate(neg), truncate(unlabeled_pos)
 
     num_folds = 10
-    kfold_pos = list(sklearn.cross_validation.KFold(pos.shape[0], k=num_folds, shuffle=True, random_state=0))
-    kfold_neg = list(sklearn.cross_validation.KFold(neg.shape[0], k=num_folds, shuffle=True, random_state=0))
-    kfold_unlabeled_pos = list(sklearn.cross_validation.KFold(unlabeled_pos.shape[0], k=num_folds, shuffle=True, random_state=0))
+    kfold_pos = list(sklearn.cross_validation.KFold(pos.shape[0], n_folds=num_folds, shuffle=True, random_state=0))
+    kfold_neg = list(sklearn.cross_validation.KFold(neg.shape[0], n_folds=num_folds, shuffle=True, random_state=0))
+    kfold_unlabeled_pos = list(sklearn.cross_validation.KFold(unlabeled_pos.shape[0], n_folds=num_folds, shuffle=True, random_state=0))
 
     for i in range(1):
         pos_indices_train, pos_indices_test = kfold_pos[i]
@@ -113,6 +106,8 @@ if __name__=='__main__':
         test_labels = create_labels((1, pos_test.shape[0] + unlabeled_pos_test.shape[0]),
                                     (0, neg_test.shape[0]))
 
+        calculate_test_roc = functools.partial(calculate_roc, test_labels)
+
         logging.debug('pos train: %s', str(pos_train.shape))
         logging.debug('neg train: %s', str(neg_train.shape))
         logging.debug('unlabeled pos train: %s', str(unlabeled_pos_train.shape))
@@ -126,39 +121,73 @@ if __name__=='__main__':
         X, y, y_labeled = sklearn.utils.shuffle(X, y, y_labeled)
         X = scipy.sparse.csr_matrix(X) # sparsify X
 
-        # Baseline if we knew everything
-        max_iter = 100
-        logging.info('starting LR on totally labeled data...')
-        theta_labeled = logistic.fast_logistic_gradient_descent(X,
-                                                                y_labeled,
-                                                                max_iter=max_iter)
-        logging.info('done LR')
+        roc_curves = []
 
-        # calculate the parameters
-        logging.info('starting LR on pos-only data...')
-        thetaR = logistic.fast_logistic_gradient_descent(X,
-                                                         y,
-                                                         max_iter=max_iter)
-        logging.info('done LR')
+        # sci-kit learn's sgd classifier
+        name = 'Sci-Kit SGD LR L2-regularized pos-only labels'
+        logging.info('starting %s on pos-only data...' % name)
+        sgd = sklearn.linear_model.SGDClassifier(loss='log', 
+                                                penalty='l2', 
+                                                alpha=0.001,
+                                                random_state=2,
+                                                n_iter=200)
+        sgd.fit(X, y)
+        sgd_labels = sgd.predict_proba(test_set)[:,1]
+        logging.info('done %s: %s' % (name, np.max(np.abs(sgd.coef_))))
+        fpr, tpr, roc_auc = calculate_test_roc(sgd_labels)
+        roc_curves.append((name, roc_auc, fpr, tpr, 'p-'))
+        logging.info('AUC for %s: %f' % (name, roc_auc))
+
+        name = 'Sci-Kit SGD LR L2-regularized true labels'
+        logging.info('starting %s on pos-only data...' % name)
+        sgd = sklearn.linear_model.SGDClassifier(loss='log', 
+                                                penalty='l2', 
+                                                alpha=0.001,
+                                                random_state=2,
+                                                n_iter=200)
+        sgd.fit(X, y_labeled)
+        sgd_labels = sgd.predict_proba(test_set)[:,1]
+        logging.info('done %s: %s' % (name, np.max(np.abs(sgd.coef_))))
+        fpr, tpr, roc_auc = calculate_test_roc(sgd_labels)
+        roc_curves.append((name, roc_auc, fpr, tpr, 'p-'))
+        logging.info('AUC for %s: %f' % (name, roc_auc))
+
+        # Baseline if we knew everything
+        max_iter = 10
         logging.info('starting modified LR on pos-only data...')
         thetaMR, b = logistic.fast_modified_logistic_gradient_descent(X,
                                                                       y, 
                                                                       max_iter=max_iter, 
                                                                       alpha=0.01)
-        logging.info('done modified LR on pos-only data')
-
+        logging.info('done modified LR on pos-only data: %s' % (np.max(np.abs(thetaMR))))
+        modified_regression_labels = logistic.label_data(test_set, thetaMR, (b * b), binarize=False)
 
         logging.info('b = %s' % b)
         logging.info('1.0 / (1.0 + b*b) = %s' % (1.0 / (1.0 + b*b)))
+        # Compute ROC curve and area the curve
+        fpr, tpr, roc_auc = calculate_test_roc(modified_regression_labels)
+        name = 'Modified LR pos-only labels'
+        roc_curves.append((name, roc_auc, fpr, tpr, 'r--'))
+        logging.info('AUC for %s: %f' % (name, roc_auc))
 
-        # label the test set
+
+        logging.info('starting LR on totally labeled data...')
+        theta_labeled = logistic.fast_logistic_gradient_descent(X,
+                                                                y_labeled,
+                                                                max_iter=max_iter)
+        logging.info('done LR')
         baseline_labels = logistic.label_data(test_set, theta_labeled, binarize=False)
+
+        # calculate the parameters
+        logging.info('starting LR on pos-only data...')
+        thetaR = logistic.fast_logistic_gradient_descent(X,
+                                                         y,
+                                                         alpha=0.01,
+                                                         max_iter=max_iter)
+        logging.info('done LR')
         regression_labels = logistic.label_data(test_set, thetaR, binarize=False)
-        modified_regression_labels = logistic.label_data(test_set, thetaMR, (b * b), binarize=False)
 
-        calculate_test_roc = functools.partial(calculate_roc, test_labels)
 
-        roc_curves = []
 
         # Compute ROC curve and area the curve
         fpr, tpr, roc_auc = calculate_test_roc(baseline_labels)
@@ -172,54 +201,56 @@ if __name__=='__main__':
         roc_curves.append((name, roc_auc, fpr, tpr, 'r-.'))
         logging.info('AUC for %s: %f' % (name, roc_auc))
 
-        # Compute ROC curve and area the curve
-        fpr, tpr, roc_auc = calculate_test_roc(modified_regression_labels)
-        name = 'Modified LR pos-only labels'
-        roc_curves.append((name, roc_auc, fpr, tpr, 'r--'))
-        logging.info('AUC for %s: %f' % (name, roc_auc))
+        calculate_svms = True
 
-        logging.info('starting SVM on pos-only data...')
-        svm_labels = svm_label_data(X, y, test_set)
-        fpr, tpr, roc_auc = calculate_test_roc(svm_labels[:,1])
-        name = 'SVM pos-only labels'
-        roc_curves.append((name, roc_auc, fpr, tpr, 'b-.'))
-        logging.info('AUC for %s: %f' % (name, roc_auc))
+        if calculate_svms:
+            logging.info('starting SVM on pos-only data...')
+            svm_labels = svm_label_data(X, y, test_set)
+            fpr, tpr, roc_auc = calculate_test_roc(svm_labels[:,1])
+            name = 'SVM pos-only labels'
+            roc_curves.append((name, roc_auc, fpr, tpr, 'b-.'))
+            logging.info('AUC for %s: %f' % (name, roc_auc))
 
-        logging.info('starting Biased SVM...')
-        svm_labels = svm_label_data(X, y, test_set, CP=C_VALUES)
-        fpr, tpr, roc_auc = calculate_test_roc(svm_labels[:,1])
-        name = 'Biased SVM pos-only labels'
-        roc_curves.append((name, roc_auc, fpr, tpr, 'g-'))
-        logging.info('AUC for %s: %f' % (name, roc_auc))
+            logging.info('starting Biased SVM...')
+            svm_labels = svm_label_data(X, y, test_set, CP=C_VALUES)
+            fpr, tpr, roc_auc = calculate_test_roc(svm_labels[:,1])
+            name = 'Biased SVM pos-only labels'
+            roc_curves.append((name, roc_auc, fpr, tpr, 'g-'))
+            logging.info('AUC for %s: %f' % (name, roc_auc))
 
-        logging.info('starting weighted SVM...')
-        # I have to copy to avoid an error that says that svm_weight is not C-contiguous!
-        svm_weight = svm_label_data(X, y, X)[:,1].copy()
-        # now run this again with the probabilites as the weights
-        svm_labels = svm_label_data(X, y, test_set, sample_weight=svm_weight)
-        fpr, tpr, roc_auc = calculate_test_roc(svm_labels[:,1])
-        name = 'Weighted SVM pos-only labels'
-        roc_curves.append((name, roc_auc, fpr, tpr, 'g--'))
-        logging.info('AUC for %s: %f' % (name, roc_auc))
+            logging.info('starting weighted SVM...')
+            # I have to copy to avoid an error that says that svm_weight is not C-contiguous!
+            svm_weight = svm_label_data(X, y, X)[:,1].copy()
+            # now run this again with the probabilites as the weights
+            svm_labels = svm_label_data(X, y, test_set, sample_weight=svm_weight, C=[0.125, 0.25, 0.5, 1.0])
+            fpr, tpr, roc_auc = calculate_test_roc(svm_labels[:,1])
+            name = 'Weighted SVM pos-only labels'
+            roc_curves.append((name, roc_auc, fpr, tpr, 'g--'))
+            logging.info('AUC for %s: %f' % (name, roc_auc))
 
-        logging.info('starting SVM on true labels...')
-        svm_labels = svm_label_data(X, y_labeled, test_set)
-        fpr, tpr, roc_auc = calculate_test_roc(svm_labels[:,1])
-        name = 'SVM true labels'
-        roc_curves.append((name, roc_auc, fpr, tpr, 'b-'))
-        logging.info('AUC for %s: %f' % (name, roc_auc))
+            logging.info('starting SVM on true labels...')
+            svm_labels = svm_label_data(X, y_labeled, test_set)
+            fpr, tpr, roc_auc = calculate_test_roc(svm_labels[:,1])
+            name = 'SVM true labels'
+            roc_curves.append((name, roc_auc, fpr, tpr, 'b-'))
+            logging.info('AUC for %s: %f' % (name, roc_auc))
 
         # Plot ROC curve
         import pylab as pl
         pl.clf()
 
-        sorted_roc_curves = reversed(sorted(roc_curves, key=lambda a: a[1]))
+        sorted_roc_curves = list(reversed(sorted(roc_curves, key=lambda a: a[1])))
         for (name, roc_auc, fpr, tpr, color) in sorted_roc_curves:
             pl.plot(fpr, tpr, color, label='%s (AUC = %0.4f)' % (name, roc_auc))
 
         pl.plot([0, 1], [0, 1], 'k--')
-        pl.xlim([0.0, 1.0])
-        pl.ylim([0.0, 1.0])
+        if sorted_roc_curves[-1][1] < 0.8:
+            # show the full curve if there are low roc_auc
+            pl.xlim([0.0, 1.0])
+            pl.ylim([0.0, 1.0])
+        else:
+            pl.xlim([0.0, 0.3])
+            pl.ylim([0.7, 1.0])
         pl.xlabel('False Positive Rate')
         pl.ylabel('True Positive Rate')
         pl.title('ROC for Protein Datasets')
